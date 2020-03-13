3264204b5f0b0c1d56c848f50a13129e06855416
SkaymanT <SkaymanT@yandex.ru>
2020-03-13 11:19:55 +0300

    feat: update DOM add HEADER

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let counter = 0;
 const MENU = document.getElementById('menu');
-MENU.querySelectorAll('a');
+MENU.addEventListener('click', (event) => {
+  MENU.querySelectorAll('a').forEach(el =>el.classList.remove('Active'));
+  event.target.classList.add('Active');
+});
